6163222dbc3367e907e517988877df782738d271
Roberta1400 <116271698+Roberta1400@users.noreply.github.com>
2023-11-02 20:54:39 +0200

    Changes
    Added 10 posts to JSON file, validated  JSON file, added JSON file to JSON Bin, added the code to Javascript so you can fetch posts information

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+const binID = '6543ea6354105e766fca8c48';
+const binVersion = 'latest';
+const apiKey = '$2a$10$96ItLli6GmLKcQ79hYBX..Sp6qKE9jKKT8VacpSidqrZVp2BC.yNC';
+
+const jsonBinURL = `https://api.jsonbin.io/v3/b/${binID}/${binVersion}`;
+
+const req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+            if (req.status === 200) {
+                console.log(JSON.parse(req.responseText));
+                //TODO: use such information to dynamically create the webpage shown in Figure 1
+            } else {
+                console.error('Failed to fetch JSON data. Status:', req.status);
+            }
+         }
+};
+
+req.open('GET', jsonBinURL, true);
+req.setRequestHeader('X-Master-Key', apiKey);
+req.send();
